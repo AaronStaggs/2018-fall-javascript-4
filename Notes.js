987ce1478b27fb40882c567fbcbd9a3585bf2df5
Aaron Staggs <aaronstaggs@d-ip-10-194-88-40.ouguest.nor.ou.edu>
2018-10-27 09:36:20 -0500

    Notes

---
+++ Notes.js
@@ -0,0 +1,37 @@
+var user1 = {
+    firstName: "Aaron",
+    lastName: "Staggs",
+    address: "888 That Road",
+    phone: "555-555-5555",
+    age: 32
+}
+var user2 = {
+    firstName: "Jackie",
+    lastName: "Staggs",
+    address: "888 That Road",
+    phone: "555-555-5555",
+    age: 29
+}
+var addressBook = [
+{
+    firstName: "Aaron",
+    lastName: "Staggs",
+    address: "888 That Road",
+    phone: "555-555-5555",
+    age: 32
+},
+{
+    firstName: "Jackie",
+    lastName: "Staggs",
+    address: "888 That Road",
+    phone: "555-555-5555",
+    age: 29
+},
+{
+    firstName: "Cory",
+    lastName: "Justice",
+    address: "726 That Other Road",
+    phone: "555-555-5555",
+    age: 32
+}
+]
