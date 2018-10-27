d4ffe69b466e903614d9a43dff768551edd11513
Aaron Staggs <aaronstaggs@d-ip-10-194-88-40.ouguest.nor.ou.edu>
2018-10-27 10:46:39 -0500

    New Page

--- Notes.js
+++ Notes.js
@@ -41,11 +41,73 @@ function printFirstNames(add){
         console.log(add[i].firstName)
     }
 }
-printFirstNames(addressBook);
+//printFirstNames(addressBook);
 
-function printAddress(add){
-    for(var i = 0; i < add.length; i++){
-        console.log(add[i].address)
+//printAddress(addressBook);
+
+// var ourFunction = function(thing){
+//     for(var i = 0; i<thing.length;i++){
+//         console.log(thing[i].lastname);
+//     };
+// }
+
+// function ourConsole(message1){
+//     return function(message2){
+//         console.log("This is our message1 out: "+message1)
+//         console.log("This is our message2 out: "+message2)
+//     }
+// }
+
+//var mgs = ourConsole();
+//mgs()
+
+function forEach(arr1, func1){
+for(var i = 0; i<arr1.length; i++){
+    func1(arr1[i]);
     }
 }
-printAddress(addressBook);
+
+
+function printName(x){
+    console.log(x.firstName, x.lastName);
+}
+
+function printAddr(x){
+    console.log(x.address);
+}
+
+
+function printContact(add){
+    for(var i = 0; i < add.length; i++){
+        console.log("===============================")
+        console.log(add[i].firstName, add[i].lastName)
+        console.log(add[i].address) 
+        console.log(add[i].phone)
+        console.log("===============================")
+        console.log(" ")
+    }
+}
+var printContact2 =function(contact){
+    console.log("===============================")
+    console.log(contact.firstName, contact.lastName)
+    console.log(contact.address) 
+    console.log(contact.phone)
+    console.log("===============================")
+    console.log(" ")
+}
+
+
+
+
+//forEach(addressBook, printName);
+//forEach(addressBook, printAddr);
+forEach(addressBook, printContact2)
+//forEach(addressBook, function(x){console.log(x.phone)})
+
+var printhello = function(){
+    console.log("Hello Everyone!!")
+}
+
+setTimeout(function(){
+    console.log("Goodbye Everyone!")
+}, 500);
